c4a19977e62717fb37452ec807666cb31acf96a3
ZATM4N <47236328+ZATM4N@users.noreply.github.com>
2025-10-02 09:16:28 +0000

    push

--- dags/xcom_taskflow.py
+++ dags/xcom_taskflow.py
@@ -3,7 +3,7 @@ import datetime
 
 from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.providers.standard.operators.python import PythonOperator
-from airflow.sdk import DAG, task
+from airflow.sdk import DAG, task, dag
 
 @task(task_id="push")
 def _push(ti):
@@ -19,10 +19,17 @@ def _pull(ti):
     print(f"This is a {another_animal}!")
 
 
-with DAG(
-    dag_id="xcom_taskflow",
+# with DAG(
+#     dag_id="xcom_taskflow",
+#     start_date=datetime.datetime(2025, 10, 1),
+#     schedule=None,
+# ):
+
+@dag(
     start_date=datetime.datetime(2025, 10, 1),
     schedule=None,
+)
+def xcom_taskflow(
 ):
     start = EmptyOperator(task_id="start")
 
@@ -43,3 +50,5 @@ with DAG(
     end = EmptyOperator(task_id="end")
 
     start >> push >> pull >> end
+
+dag = xcom_taskflow()
