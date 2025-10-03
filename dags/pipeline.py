fbe839584d8927860950640cf7e4728fcbc1c52a
ZATM4N <47236328+ZATM4N@users.noreply.github.com>
2025-10-03 07:53:06 +0000

    push

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -7,11 +7,19 @@ from airflow.providers.standard.operators.empty import EmptyOperator
 from airflow.providers.standard.operators.python import PythonOperator
 from airflow.sdk import DAG
 
-def _list_customers():
+def _extract_customer_data():
     pg_hook = PostgresHook(postgres_conn_id="my_postgres_connection")
     customers_df = pg_hook.get_df(
         "SELECT * FROM customers;"
     )
+    logging.info(customers_df)
+    return customers_df
+
+def _transform_data():
+    #customers_df = pd.read_csv(...)
+    # Format the datetime from "12 May 1990" to "1990-05-12"
+    customers_df["birthdate"] = pd.to_datetime(df["birthdate"], dayfirst=True)    
+    
     logging.info(customers_df)
     return customers_df
 
@@ -22,9 +30,14 @@ with DAG(
 ):
     start = EmptyOperator(task_id="start")
 
-    list_customers = PythonOperator(
-        task_id="list_customers",
-        python_callable=_list_customers,
+    extract_customer_data = PythonOperator(
+        task_id="extract_customer_data",
+        python_callable=_extract_customer_data,
+    )
+
+    transform_data = PythonOperator(
+        task_id="transform_data",
+        python_callable=_transform_data,
     )
 
     end = EmptyOperator(task_id="end")
