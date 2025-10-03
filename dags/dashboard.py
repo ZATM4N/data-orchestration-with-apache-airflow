4a570c584e82fbecd03e9bcf9e9b025dd609f975
ZATM4N <47236328+ZATM4N@users.noreply.github.com>
2025-10-03 03:31:38 +0000

    push

---
+++ dags/dashboard.py
@@ -0,0 +1,20 @@
+from airflow.sdk import DAG
+from airflow.utils import timezone
+from airflow.providers.standard.operators.empty import EmptyOperator
+
+
+with DAG(
+    "dashboard",
+    start_date=timezone.datetime(2025, 10, 2),
+    schedule=None,
+):
+    start = EmptyOperator(task_id="start")
+    extract_api = EmptyOperator(task_id="extract_api")
+    extract_postgres = EmptyOperator(task_id="extract_postgres")
+    extract_google_analytics = EmptyOperator(task_id="extract_google_analytics")
+    tranform_and_clean_data = EmptyOperator(task_id="tranform_and_clean_data")
+    load_to_data_warehouse = EmptyOperator(task_id="load_to_data_warehouse")
+    generate_reports = EmptyOperator(task_id="generate_reports")
+    end = EmptyOperator(task_id="end")
+
+    start >> [extract_api, extract_postgres, extract_google_analytics] >> tranform_and_clean_data >> load_to_data_warehouse >> generate_reports >> end
